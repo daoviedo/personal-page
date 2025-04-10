2fc0cbce80f62e48ea619d36f98852e1e1179749
Daniel Oviedo <daniel.a2006@hotmail.com>
2025-04-10 00:32:32 -0500

    added animations and react router for sections

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,26 @@
 import { FiMoon, FiSun } from 'react-icons/fi';
 import { useTheme } from '../theme';
 import { Button } from './button';
+import { ScrollLink } from './scroll-link';
 
 function Header() {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className="w-full h-14 flex fixed items-center justify-center text-sm font-medium bg-light-primary/50 dark:bg-dark-primary/50 backdrop-blur-xs z-11000">
-      <Button className="text-light-text-secondary hover:text-light-text-primary hover:cursor-pointer dark:text-dark-text-secondary dark:hover:text-dark-text-primary p-1 ml-2.5 mr-2.5">
+      <ScrollLink
+        to="#about"
+        offset={-80}
+        className="text-light-text-secondary hover:text-light-text-primary hover:cursor-pointer dark:text-dark-text-secondary dark:hover:text-dark-text-primary p-1 ml-2.5 mr-2.5"
+      >
         About
-      </Button>
-      <Button className="text-light-text-secondary hover:text-light-text-primary hover:cursor-pointer dark:text-dark-text-secondary dark:hover:text-dark-text-primary p-1 ml-2.5 mr-2.5">
-        Projects
-      </Button>
+      </ScrollLink>
+      <ScrollLink
+        to="#experience"
+        offset={-80}
+        className="text-light-text-secondary hover:text-light-text-primary hover:cursor-pointer dark:text-dark-text-secondary dark:hover:text-dark-text-primary p-1 ml-2.5 mr-2.5"
+      >
+        Experience
+      </ScrollLink>
       <Button
         className="text-light-text-secondary hover:text-light-text-primary hover:cursor-pointer dark:text-dark-text-secondary dark:hover:text-dark-text-primary p-1 ml-2.5 mr-2.5"
         onClick={toggleTheme}
--- src/components/skill-card.tsx
+++ src/components/skill-card.tsx
@@ -2,11 +2,15 @@ import { Skill } from './skills-util';
 
 interface SkillCardProps {
   skill: Skill;
+  index: number;
 }
 
-export const SkillCard = ({ skill }: SkillCardProps) => {
+export const SkillCard = ({ skill, index }: SkillCardProps) => {
   return (
-    <div className="card">
+    <div
+      className="card motion-preset-blur-right"
+      style={{ animationDelay: `${index * 200 + 300}ms` }}
+    >
       <div className="text-4xl">{skill.component}</div>
       <p className="ml-1">{skill.name}</p>
     </div>
